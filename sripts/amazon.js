3343e94496dc28db5c1b9c27885daf78bd013bda
Luka <lgurjidze@gmail.com>
2024-02-20 21:43:10 +0400

    add to cart functionality

--- sripts/amazon.js
+++ sripts/amazon.js
@@ -1,67 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 pairs',
-    rating: {
-        stars: 4.5,
-        count: 87,
-    },
-    priceCents: 1090 //cents 
-},
-{
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-    keywords: [
-      "sports",
-      "basketballs"
-    ]
-  },
-
-
-  {
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-    keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-  },
-
-
-
-  {
-    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-    image: "images/products/black-2-slot-toaster.jpg",
-    name: "2 Slot Toaster - Black",
-    rating: {
-      stars: 5,
-      count: 2197
-    },
-    priceCents: 1899,
-    keywords: [
-      "toaster",
-      "kitchen",
-      "appliances"
-    ]
-  },
-
-
-];
-
 let productsHTML = '';
 
 
@@ -110,7 +46,8 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart"
+    data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>`;
@@ -121,3 +58,34 @@ products.forEach((product) => {
 
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            let mathingItem;
+            const productId = button.dataset.productId;
+
+            cart.forEach((item) =>{
+                if (productId === item.productId) {
+                    mathingItem = item;
+                }
+            })
+
+
+            if (mathingItem) {
+                mathingItem.quantity += 1;
+            }
+
+            else {
+                cart.push({
+                    productId: productId,
+                    quantity: 1,
+                })
+            }
+
+            
+        })
+    
+})
+
+console.log(cart)
